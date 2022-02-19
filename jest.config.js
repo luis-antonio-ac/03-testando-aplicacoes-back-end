bb61b941174ea011b805dbb4b86df676e1a8bf72
Luis Antonio <luis.developer.ac@gmail.com>
2022-02-19 18:39:29 -0500

    chore(jest): add utils to moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 export default {
   setupFiles: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    "^controllers/(.*)$": "<rootdir>/src/controllers/$1",
-    "^middlewares/(.*)$": "<rootdir>/src/middlewares/$1",
-    "^services/(.*)$": "<rootdir>/src/services/$1",
+    "^controllers/(.*)$": "<rootDir>/src/controllers/$1",
+    "^middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
+    "^services/(.*)$": "<rootDir>/src/services/$1",
+    "^utils/(.*)$": "<rootDir>/src/utils/$1",
   },
+  testMatch: ["**/*.unit.js", "**/__tests__/*.js?(x)"],
 };
