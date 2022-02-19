947a08777c5b4f80fc30f34057bf03a1f85a4347
Luis Antonio <luis.developer.ac@gmail.com>
2022-02-19 16:08:44 -0500

    chore(jest): create jest config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  setupFiles: ["<rootdir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^controllers/(.*)$": "<rootdir>/src/controllers/$1",
+    "^middlewares/(.*)$": "<rootdir>/src/middlewares/$1",
+    "^services/(.*)$": "<rootdir>/src/services/$1",
+  },
+};
