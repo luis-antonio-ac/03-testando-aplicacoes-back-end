616752ec29b88f515270821daa41f8fd32615013
Luis Antonio <luis.developer.ac@gmail.com>
2022-02-20 22:08:59 -0500

    test(controllers): create tests for create user

--- src/controllers/user.controller.unit.js
+++ src/controllers/user.controller.unit.js
@@ -1,4 +1,4 @@
-import { list } from "./user.controller";
+import { list, create } from "./user.controller";
 import { createAuth, createRequest, createResponse } from "utils/create";
 
 import findUser, { basedOnQuery } from "services/user/find";
@@ -6,6 +6,13 @@ jest.mock("services/user/find");
 
 import logger from "@jsassertivo/cli/src/utils/logger.js";
 jest.mock("@jsassertivo/cli/src/utils/logger.js");
+
+import { createUser } from "services/user/create.js";
+jest.mock("services/user/create");
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe("Controller: list", () => {
   it("should return a list of users", async () => {
@@ -40,5 +47,41 @@ describe("Controller: list", () => {
     expect(logger.error.mock.calls).toEqual([
       ["Ocorreu um erro ao listar usuários", new Error("500")],
     ]);
+  });
+});
+
+describe("Controller: create", () => {
+  it("should create the user", async () => {
+    const user = createAuth({ uid: "abc-123" });
+    const request = createRequest({ body: user });
+    const response = createResponse();
+
+    createUser.mockResolvedValueOnce(user);
+
+    await create(request, response);
+
+    expect(createUser.mock.calls).toEqual([[user]]);
+
+    expect(response.json).toHaveBeenCalledTimes(1);
+    expect(response.json).toBeCalledWith(user);
+  });
+
+  it("should throw 500", async () => {
+    const user = createAuth({ uid: "abc-123" });
+    const request = createRequest({ body: user });
+    const response = createResponse();
+
+    createUser.mockRejectedValueOnce(new Error(500));
+
+    await create(request, response);
+
+    expect(createUser.mock.calls).toEqual([[user]]);
+
+    expect(logger.error.mock.calls).toEqual([
+      ["Ocorreu um erro ao criar usuário", new Error(500)],
+    ]);
+    expect(response.status.mock.calls).toEqual([[500]]);
+    expect(response.json).toHaveBeenCalledTimes(1);
+    expect(response.json).toBeCalledWith({ message: "500" });
   });
 });
